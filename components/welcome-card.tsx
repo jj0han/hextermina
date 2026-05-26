6236b0f2a6f4f045cb9f04362a2c67a18ee71518
jj0han <joaovieirabomfim@gmail.com>
2026-05-26 16:23:26 -0300

    Add typography components and update WelcomeCard UI

--- components/welcome-card.tsx
+++ components/welcome-card.tsx
@@ -1,54 +1,116 @@
 "use client"
-
-import { Moon02Icon, Sun01Icon } from "@hugeicons/core-free-icons"
+import {
+  ExternalLink,
+  GithubIcon,
+  Moon02Icon,
+  PaintBrush04Icon,
+  Sun01Icon,
+} from "@hugeicons/core-free-icons"
 import { HugeiconsIcon } from "@hugeicons/react"
 import { useQuery } from "@tanstack/react-query"
 import { useTheme } from "next-themes"
+import { useEffect, useState } from "react"
 
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemGroup,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item"
+import {
+  TypographyH1,
+  TypographyInlineCode,
+  TypographyLead,
+  TypographyP,
+} from "@/components/ui/typography"
 import { useTRPC } from "@/trpc/client"
 
-import { Button } from "./ui/button"
+import { Badge } from "./ui/badge"
 
 export function WelcomeCard() {
+  const [mounted, setMounted] = useState(false)
   const { theme, setTheme } = useTheme()
   const trpc = useTRPC()
   const greeting = useQuery(trpc.hello.list.queryOptions({ text: "world" }))
 
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark")
+  }
+
+  // Avoid hydration mismatch by only rendering on the client because of the theme toggle
+  useEffect(() => {
+    //eslint-disable-next-line react-hooks/set-state-in-effect
+    setMounted(true)
+  }, [])
+
+  if (!mounted) {
+    return null
+  }
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-background p-6">
-      <div className="w-full max-w-sm min-w-0">
+      <div className="w-full max-w-lg min-w-0 space-y-6">
+        <TypographyH1>Project ready!</TypographyH1>
+        <TypographyLead>
+          You may now start building. We&apos;ve already added the components
+          for you.
+        </TypographyLead>
         <Card>
           <CardHeader>
-            <CardTitle>Project ready!</CardTitle>
-            <CardDescription>
-              <p>You may now add components and start building.</p>
-              <p>We&apos;ve already added the button component for you.</p>
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            Prefetching TRPC query: {greeting.data?.greeting}
-          </CardContent>
-          <CardFooter>
-            <Button
-              size="icon"
-              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            <CardTitle>Prefetch query test</CardTitle>
+            <Badge
+              variant={greeting.status === "success" ? "default" : "secondary"}
             >
-              {theme === "dark" ? (
-                <HugeiconsIcon icon={Sun01Icon} strokeWidth={2} />
-              ) : (
-                <HugeiconsIcon icon={Moon02Icon} strokeWidth={2} />
-              )}
-            </Button>
-          </CardFooter>
+              {greeting.status}
+            </Badge>
+          </CardHeader>
+          <CardContent className="flex gap-2">
+            <TypographyP>data:</TypographyP>
+            <TypographyInlineCode>
+              {greeting.data?.greeting}
+            </TypographyInlineCode>
+          </CardContent>
         </Card>
+        <ItemGroup>
+          <Item variant="outline">
+            <ItemMedia variant="icon">
+              <HugeiconsIcon icon={PaintBrush04Icon} strokeWidth={2} />
+            </ItemMedia>
+            <ItemContent>
+              <ItemTitle>Theme</ItemTitle>
+              <ItemDescription>
+                Toggle between light and dark mode.
+              </ItemDescription>
+            </ItemContent>
+            <ItemActions>
+              <Button size="icon" variant="secondary" onClick={toggleTheme}>
+                <HugeiconsIcon
+                  icon={theme === "dark" ? Sun01Icon : Moon02Icon}
+                  strokeWidth={2}
+                />
+              </Button>
+            </ItemActions>
+          </Item>
+          <Item variant="outline">
+            <ItemMedia variant="icon">
+              <HugeiconsIcon icon={GithubIcon} strokeWidth={2} />
+            </ItemMedia>
+            <ItemContent>
+              <ItemTitle>Visit the documentation</ItemTitle>
+              <ItemDescription>Learn how to get started.</ItemDescription>
+            </ItemContent>
+            <ItemActions>
+              <Button size="icon" variant="secondary">
+                <HugeiconsIcon icon={ExternalLink} strokeWidth={2} />
+              </Button>
+            </ItemActions>
+          </Item>
+        </ItemGroup>
       </div>
     </div>
   )
